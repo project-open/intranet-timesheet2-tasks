7fa630162498eb8094043ee8923d46b9bbe89bb1
Frank Bergmann <frank.bergmann@project-open.com>
2005-08-12 19:16:44 +0000

    added deleting tasks

--- sql/postgresql/intranet-timesheet2-tasks-create.sql
+++ sql/postgresql/intranet-timesheet2-tasks-create.sql
@@ -385,6 +385,11 @@ insert into im_view_columns (column_id, view_id, group_id, column_name, column_r
 extra_select, extra_where, sort_order, visible_for) values (91110,911,NULL,'UoM',
 '$uom','','',10,'');
 
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (91112,911,NULL, 
+'"[im_gif del "Delete"]"', 
+'"<input type=checkbox name=task_id.$task_id>"', '', '', 12, '');
+
 
 
 
